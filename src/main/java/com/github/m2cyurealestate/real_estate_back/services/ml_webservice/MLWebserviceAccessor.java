54d476dcdcd713ea4527f5379080bf581b884864
AldricVS <aldric.silvestre@outlook.fr>
2023-06-15 16:33:53 +0200

    Increase webservice client timeout

--- src/main/java/com/github/m2cyurealestate/real_estate_back/services/ml_webservice/MLWebserviceAccessor.java
+++ src/main/java/com/github/m2cyurealestate/real_estate_back/services/ml_webservice/MLWebserviceAccessor.java
@@ -1,8 +1,12 @@
 package com.github.m2cyurealestate.real_estate_back.services.ml_webservice;
 
 import com.github.m2cyurealestate.real_estate_back.config.properties.WebserviceProperties;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 /**
  * The main accessor for the smart webservice
@@ -15,8 +19,13 @@ public class MLWebserviceAccessor {
     private final WebClient webclient;
 
     public MLWebserviceAccessor(WebserviceProperties webserviceProperties) {
+        // Set the http client configuration
+        HttpClient client = HttpClient.create()
+                .responseTimeout(Duration.ofMinutes(5));
+
         webclient = WebClient.builder()
                 .baseUrl(webserviceProperties.url())
+                .clientConnector(new ReactorClientHttpConnector(client))
                 .build();
     }
 
